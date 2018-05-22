300dec3f97a0e3880874ab343f6e9503f638e5ce
EigenLaw <34590153+EigenLaw@users.noreply.github.com>
2018-05-22 20:30:11 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import re
+import io
+from setuptools import setup
+from setuptools.extension import Extension
+
+
+setup(
+    author="Eigen",
+    author_email="eigenluo@gmail.com",
+    name='Pdf2Excel',
+    version="1",
+    url='https://github.com/EigenLaw/Pdf2Excel',
+    description='Turn pdf into excel file using ABBYY API',
+    license='MIT',
+    install_requires=['time', 'argparse', 'AbbyyOnlineSdk'],
+    #ext_modules=[Extension("wordcloud.query_integral_image",
+    #                       ["wordcloud/query_integral_image.c"])],
+    scripts=['Pdf2Excel.py'],
+    packages=['Pdf2Excel'],
+    #package_data={'wordcloud': ['stopwords', 'DroidSansMono.ttf']}
+)
